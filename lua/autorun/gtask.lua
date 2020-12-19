05fc838673e131ccfef5ab4ecb22b97216e96efc
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-19 18:47:47 +0700

    Update gtask.lua

--- lua/autorun/gtask.lua
+++ lua/autorun/gtask.lua
@@ -1,7 +1,7 @@
 --[[
 MIT License
 
-Copyright (c) 2020 Aleksandrs Filipovskis
+Copyright (c) 2020 Aleksandrs Filipovskis & Be1zebub
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
